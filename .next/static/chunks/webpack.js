04f44428772d73caa00c6578f40cdded42ca94e0
anamontiaga <amontiaga@gmail.com>
2023-09-05 16:46:33 +0200

    delete test and clean readme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd3232152c28b210"; }
+/******/ 		__webpack_require__.h = function() { return "4f6f966655fe28ec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
