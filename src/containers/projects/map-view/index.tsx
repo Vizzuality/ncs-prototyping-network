14b353997faad5eb8f2663d548647145b0bf9d02
anamontiaga <amontiaga@gmail.com>
2023-09-26 11:03:01 +0200

    no data cases in project

--- src/containers/projects/map-view/index.tsx
+++ src/containers/projects/map-view/index.tsx
@@ -25,30 +25,39 @@ const MapView = ({ data }: { data: Project[] }): JSX.Element => {
   return (
     <AnimatePresence>
       <motion.div
-        className="z-20 w-6/12"
+        className="z-20"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ delay: 0.25, duration: 0.3 }}
       >
-        <div className="flex items-center justify-end space-x-6">
-          <p className="font-sans text-xs text-text">SORT BY:</p>
-          <div className="mb-1">
-            <Select
-              theme="secondary"
-              type="Country"
-              onValueChange={(v) => setSortedBy(v)}
-              options={SORT_OPTIONS}
-            />
+        {!sortedData.length && (
+          <div className="flex h-64 w-full items-center justify-center">
+            <p className="font-serif text-lg font-semibold text-indigo">No projects found</p>
           </div>
-        </div>
-        <div className="grid grid-cols-2 gap-3">
-          {sortedData.map((project) => (
-            <div key={project.id}>
-              <Card data={project} />
+        )}
+        {!!sortedData.length && (
+          <div className="w-6/12">
+            <div className="flex items-center justify-end space-x-6">
+              <p className="font-sans text-xs text-text">SORT BY:</p>
+              <div className="mb-1">
+                <Select
+                  theme="secondary"
+                  type="Country"
+                  onValueChange={(v) => setSortedBy(v)}
+                  options={SORT_OPTIONS}
+                />
+              </div>
             </div>
-          ))}
-        </div>
+            <div className="grid grid-cols-2 gap-3">
+              {sortedData.map((project) => (
+                <div key={project.id}>
+                  <Card data={project} />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </motion.div>
     </AnimatePresence>
   );
