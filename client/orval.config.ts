563d47a4c01dbf18c47c8f6c03897f682d74bdfc
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2023-10-09 09:19:50 +0200

    rescues missing files

---
+++ client/orval.config.ts
@@ -0,0 +1,29 @@
+module.exports = {
+  hrt: {
+    output: {
+      mode: 'tags',
+      client: 'react-query',
+      target: './src/types/generated/strapi.ts',
+      mock: false,
+      clean: true,
+      prettier: true,
+      override: {
+        mutator: {
+          path: './src/services/api/index.ts',
+          name: 'API',
+        },
+        query: {
+          useQuery: true,
+          useMutation: true,
+          signal: true,
+        },
+      },
+    },
+    input: {
+      target: '../cms/src/extensions/documentation/documentation/1.0.0/full_documentation.json',
+      filters: {
+        tags: ['Project'],
+      },
+    },
+  },
+};
