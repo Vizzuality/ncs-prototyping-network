c316b2c1f0476ba0fb6930ce479c7c461be59ae8
anamontiaga <amontiaga@gmail.com>
2023-10-17 10:24:50 +0200

    meta tags

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,20 +1,28 @@
 'use client';
 import 'styles/globals.css';
-import Head from 'next/head';
+
+import { usePathname } from 'next/navigation';
+
+import MetaTags from '@/containers/meta-tags';
 
 import Providers from 'app/layout-providers';
 import { Toaster } from 'components/ui/toaster';
 import Header from 'containers/header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   return (
     <Providers>
       <html lang="en">
         <body className="font-notes min-h-screen w-screen">
-          <Head>
-            <title>TNC Prototype Dashboard</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
+          <MetaTags
+            name="NCS Prototyping Network"
+            title="NCS Prototyping Network"
+            description="The Nature Conservancy is partnering with on-the-ground practitioners to help activate natural climate solutions (NCS). This Prototyping Network is field testing and evaluating high-impact strategies to be scaled around the world."
+            url={`${process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_URL}${pathname}`}
+            type="article"
+          />
           <main className="flex min-h-screen flex-col">
             <Toaster />
             <Header />
---
+++ client/src/containers/meta-tags/types.d.ts
@@ -0,0 +1,15 @@
+export interface MetaTagsProps {
+  name: string;
+  title: string;
+  description: string;
+  url?: string;
+  type: string;
+  publisher?: string;
+  section?: string;
+  tag?: string;
+  imageURL?: string;
+  imageWidth?: string;
+  imageHeight?: string;
+  twitterCard?: string;
+  twitterSite?: string;
+}
