3bac2152ed9c37a728ee86b5551f625a4e431a0c
anamontiaga <amontiaga@gmail.com>
2023-10-17 11:46:01 +0200

    set meta directly on layouts

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,28 +1,22 @@
 'use client';
 import 'styles/globals.css';
-
-import { usePathname } from 'next/navigation';
-
-import MetaTags from '@/containers/meta-tags';
+import Head from 'next/head';
 
 import Providers from 'app/layout-providers';
 import { Toaster } from 'components/ui/toaster';
 import Header from 'containers/header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-
   return (
     <Providers>
       <html lang="en">
         <body className="font-notes min-h-screen w-screen">
-          <MetaTags
-            name="NCS Prototyping Network"
-            title="NCS Prototyping Network"
-            description="The Nature Conservancy is partnering with on-the-ground practitioners to help activate natural climate solutions (NCS). This Prototyping Network is field testing and evaluating high-impact strategies to be scaled around the world."
-            url={`${process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_URL}${pathname}`}
-            type="article"
-          />
+          <Head>
+            <title>TNC Prototype Dashboard</title>
+            <link rel="icon" href="/favicon.ico" />
+            <meta name="viewport" content="width=1024" />
+          </Head>
+
           <main className="flex min-h-screen flex-col">
             <Toaster />
             <Header />
