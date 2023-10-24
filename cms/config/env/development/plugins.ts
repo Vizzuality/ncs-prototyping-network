87c8f5e7309788c33e069b756dd65d53a449cc59
Tiago Garcia <tiagojsag@gmail.com>
2023-10-24 10:46:48 +0200

    New image upload breakpoints

---
+++ cms/config/env/development/plugins.ts
@@ -0,0 +1,63 @@
+module.exports = ({ env }) => ({
+  "string-array": {
+    enabled: true,
+  },
+  email: {
+    config: {
+      provider: 'amazon-ses',
+      providerOptions: {
+        key: env('AWS_SES_ACCESS_KEY_ID'),
+        secret: env('AWS_SES_ACCESS_KEY_SECRET'),
+        amazon: `https://email.${env('AWS_REGION')}.amazonaws.com`,
+      },
+      settings: {
+        defaultFrom: `no-reply@no-reply.${env('AWS_SES_DOMAIN')}`,
+        defaultReplyTo: `no-reply@no-reply.${env('AWS_SES_DOMAIN')}`,
+      },
+    },
+  },
+  upload: {
+    config: {
+      sizeLimit: 200 * 1024 * 1024,
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
+      },
+      provider: 'local',
+    },
+  },
+  documentation: {
+    config: {
+      "x-strapi-config": {
+        mutateDocumentation: (generatedDocumentationDraft) => {
+          console.log("generatedDocumentationDraft", generatedDocumentationDraft);
+          Object.keys(generatedDocumentationDraft.paths).forEach((path) => {
+            // check if it has {id} in the path
+            if (path.includes("{id}")) {
+              // add `populate` as params
+              if (generatedDocumentationDraft.paths[path].get) {
+                if (!generatedDocumentationDraft.paths[path].get.parameters.find((param) => param.name === "populate")) {
+                  generatedDocumentationDraft.paths[path].get.parameters.push(
+                    {
+                      "name": "populate",
+                      "in": "query",
+                      "description": "Relations to return",
+                      "deprecated": false,
+                      "required": false,
+                      "schema": {
+                        "type": "string"
+                      }
+                    },
+                  );
+                }
+              }
+            }
+          });
+        },
+      },
+    },
+  },
+});
--- cms/config/env/production/plugins.ts
+++ cms/config/env/production/plugins.ts
@@ -18,6 +18,14 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
+      sizeLimit: 200 * 1024 * 1024, // 200mb
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
+      },
       provider: 'aws-s3',
       providerOptions: {
         s3Options: {
