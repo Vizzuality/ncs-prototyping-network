d1b71f70870f27734b5b80a74dee1c41bf8abc0c
anamontiaga <amontiaga@gmail.com>
2024-01-25 15:07:49 +0100

    task-52

--- client/src/containers/projects/metrics-view/index.tsx
+++ client/src/containers/projects/metrics-view/index.tsx
@@ -63,145 +63,157 @@ const MetricsView = ({ data }: { data }): JSX.Element => {
           </div>
         )}
         {!!sortedData.length && (
-          <table className="bg-white text-xs">
-            <thead className="h-16 bg-white">
-              <tr className="bg-white text-left [&>*]:px-4 [&>*]:py-2">
-                {COLUMNS.map((column) => (
-                  <th
-                    key={column.id}
-                    className="sticky top-[198px] z-30 cursor-pointer border-b border-indigo bg-white"
-                    style={{ width: column.width }}
-                    onClick={() => {
-                      if (column.sorting) {
-                        setSortedBy(column.id);
-                        setDirection(direction === 'asc' ? 'desc' : 'asc');
-                      }
-                    }}
-                  >
-                    <div
-                      className={cn({
-                        'mb-2 flex items-end font-sans text-m font-normal leading-4 text-text':
-                          true,
-                        'text-indigo': column.sorting,
-                      })}
+          <>
+            <table className="bg-white text-xs">
+              <thead className="h-16 bg-white">
+                <tr className="bg-white text-left [&>*]:px-4 [&>*]:py-2">
+                  {COLUMNS.map((column) => (
+                    <th
+                      key={column.id}
+                      className="sticky top-[198px] z-30 cursor-pointer border-b border-indigo bg-white"
+                      style={{ width: column.width }}
+                      onClick={() => {
+                        if (column.sorting) {
+                          setSortedBy(column.id);
+                          setDirection(direction === 'asc' ? 'desc' : 'asc');
+                        }
+                      }}
                     >
-                      <p>{column.label}</p>
-
-                      {column.sorting && (
-                        <span>
-                          {sortedBy === column.id && direction === 'asc' && (
-                            <HiChevronUp className="-mb-1 fill-butternut" size={25} />
-                          )}
-                          {sortedBy === column.id && direction === 'desc' && (
-                            <HiChevronDown className="-mb-1 fill-butternut" size={25} />
-                          )}
-                          {sortedBy !== column.id && (
-                            <HiChevronUp className="-mb-1 fill-butternut" size={25} />
-                          )}
-                        </span>
-                      )}
-                    </div>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody className="[&>*]:h-10">
-              {sortedData.map((project) => {
-                const CO_BENEFITS_ORDERED = project.attributes.cobenefits.data
-                  .map((cb) => cb)
-                  .sort(
-                    (a, b) =>
-                      CO_BENEFITS_ORDER[a.attributes.name] - CO_BENEFITS_ORDER[b.attributes.name]
-                  );
-
-                return (
-                  <tr
-                    key={project.id}
-                    className="text-2xs text-text [&>*]:border-b-[2px] [&>*]:px-4 [&>*]:py-4"
-                  >
-                    <td className="max-w-[140px] !pl-0 xl:w-3/12 xl:max-w-0">
-                      <Link
-                        href={`/projects/${toSlug(project.attributes.project_name)}`}
-                        className="group flex flex-col space-y-3 xl:flex-row xl:space-y-0 xl:space-x-3"
-                      >
-                        <Image
-                          alt={project.attributes.header_photo.data.attributes.formats.medium?.name}
-                          src={
-                            project.attributes.header_photo.data.attributes.formats.medium?.url ||
-                            'https://dummyimage.com/110x110/000/fff&text=+'
-                          }
-                          style={{
-                            objectFit: 'cover',
-                            height: '100px',
-                            minWidth: '100px',
-                          }}
-                          height={110}
-                          width={110}
-                        />
-
-                        <div className="flex w-[100px] flex-col xl:w-auto">
-                          <p className="-mt-1 font-serif text-lg font-semibold leading-6 text-indigo group-hover:underline xl:text-2xl xl:leading-7">
-                            {project.attributes.project_name}
-                          </p>
-                          <p className="overflow-hidden text-2xs text-text group-hover:opacity-80">
-                            {project.attributes.long_title}
-                          </p>
-                        </div>
-                      </Link>
-                    </td>
-                    <td className="space-y-1 bg-background">
-                      {project.attributes.pathways.data
-                        .map((p) => p.attributes.name)
-                        .map((pathway, idx) => (
-                          <p key={idx}>{pathway}</p>
-                        ))}
-                    </td>
-                    <td>
-                      <div>
-                        {project.attributes.action_types.data
-                          .map((pa) => pa.attributes.name)
-                          .map((at, idx) => (
-                            <p key={idx}>{at}</p>
-                          ))}
-                      </div>
-                    </td>
-                    <td className="space-y-1 bg-background">
-                      {project.attributes.project_phases.data
-                        .map((pp) => pp.attributes.name)
-                        .map((phase, idx) => (
-                          <p key={idx}>{phase}</p>
-                        ))}
-                    </td>
-                    <td className="space-y-1">
-                      {project.attributes.project_categories.data
-                        .map((pc) => pc.attributes.name)
-                        .map((category, idx) => (
-                          <p key={idx}>{category}</p>
-                        ))}
-                    </td>
-                    <td className="bg-background">{toTBD(project.attributes.hectares_impacted)}</td>
-                    <td>{toTBD(project.attributes.people_supported)}</td>
-                    <td className="bg-background">{toTBD(project.attributes.carbon_mitigation)}</td>
-                    <td>
-                      <div className="grid grid-cols-2 gap-x-0 gap-y-5">
-                        {CO_BENEFITS_ORDERED.map((cb) => {
-                          return (
-                            <Image
-                              alt={cb}
-                              src={CO_BENEFITS_ICONS[cb.attributes.name]}
-                              width={28}
-                              height={28}
-                              key={cb.attributes.name}
-                            />
-                          );
+                      <div
+                        className={cn({
+                          'mb-2 flex items-end font-sans text-m font-normal leading-4 text-text':
+                            true,
+                          'text-indigo': column.sorting,
                         })}
+                      >
+                        <p>{column.label}</p>
+
+                        {column.sorting && (
+                          <span>
+                            {sortedBy === column.id && direction === 'asc' && (
+                              <HiChevronUp className="-mb-1 fill-butternut" size={25} />
+                            )}
+                            {sortedBy === column.id && direction === 'desc' && (
+                              <HiChevronDown className="-mb-1 fill-butternut" size={25} />
+                            )}
+                            {sortedBy !== column.id && (
+                              <HiChevronUp className="-mb-1 fill-butternut" size={25} />
+                            )}
+                          </span>
+                        )}
                       </div>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody className="[&>*]:h-10">
+                {sortedData.map((project) => {
+                  const CO_BENEFITS_ORDERED = project.attributes.cobenefits.data
+                    .map((cb) => cb)
+                    .sort(
+                      (a, b) =>
+                        CO_BENEFITS_ORDER[a.attributes.name] - CO_BENEFITS_ORDER[b.attributes.name]
+                    );
+
+                  return (
+                    <tr
+                      key={project.id}
+                      className="text-2xs text-text [&>*]:border-b-[2px] [&>*]:px-4 [&>*]:py-4"
+                    >
+                      <td className="max-w-[140px] !pl-0 xl:w-3/12 xl:max-w-0">
+                        <Link
+                          href={`/projects/${toSlug(project.attributes.project_name)}`}
+                          className="group flex flex-col space-y-3 xl:flex-row xl:space-y-0 xl:space-x-3"
+                        >
+                          <Image
+                            alt={
+                              project.attributes.header_photo.data.attributes.formats.medium?.name
+                            }
+                            src={
+                              project.attributes.header_photo.data.attributes.formats.medium?.url ||
+                              'https://dummyimage.com/110x110/000/fff&text=+'
+                            }
+                            style={{
+                              objectFit: 'cover',
+                              height: '100px',
+                              minWidth: '100px',
+                            }}
+                            height={110}
+                            width={110}
+                          />
+
+                          <div className="flex w-[100px] flex-col xl:w-auto">
+                            <p className="-mt-1 font-serif text-lg font-semibold leading-6 text-indigo group-hover:underline xl:text-2xl xl:leading-7">
+                              {project.attributes.project_name}
+                            </p>
+                            <p className="overflow-hidden text-2xs text-text group-hover:opacity-80">
+                              {project.attributes.long_title}
+                            </p>
+                          </div>
+                        </Link>
+                      </td>
+                      <td className="space-y-1 bg-background">
+                        {project.attributes.pathways.data
+                          .map((p) => p.attributes.name)
+                          .map((pathway, idx) => (
+                            <p key={idx}>{pathway}</p>
+                          ))}
+                      </td>
+                      <td>
+                        <div>
+                          {project.attributes.action_types.data
+                            .map((pa) => pa.attributes.name)
+                            .map((at, idx) => (
+                              <p key={idx}>{at}</p>
+                            ))}
+                        </div>
+                      </td>
+                      <td className="space-y-1 bg-background">
+                        {project.attributes.project_phases.data
+                          .map((pp) => pp.attributes.name)
+                          .map((phase, idx) => (
+                            <p key={idx}>{phase}</p>
+                          ))}
+                      </td>
+                      <td className="space-y-1">
+                        {project.attributes.project_categories.data
+                          .map((pc) => pc.attributes.name)
+                          .map((category, idx) => (
+                            <p key={idx}>{category}</p>
+                          ))}
+                      </td>
+                      <td className="bg-background">
+                        {toTBD(project.attributes.hectares_impacted)}
+                      </td>
+                      <td>{toTBD(project.attributes.people_supported)}</td>
+                      <td className="bg-background">
+                        {toTBD(project.attributes.carbon_mitigation)}
+                      </td>
+                      <td>
+                        <div className="grid grid-cols-2 gap-x-0 gap-y-5">
+                          {CO_BENEFITS_ORDERED.map((cb) => {
+                            return (
+                              <Image
+                                alt={cb}
+                                src={CO_BENEFITS_ICONS[cb.attributes.name]}
+                                width={28}
+                                height={28}
+                                key={cb.attributes.name}
+                              />
+                            );
+                          })}
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <p className="py-3 text-xs font-normal text-text/50">
+              <span className="text-sm">*</span> Mitigation values presented may or may not be
+              equivalent to carbon credit potential depending on methodology and timeframe.
+            </p>
+          </>
         )}
       </motion.div>
     </AnimatePresence>
