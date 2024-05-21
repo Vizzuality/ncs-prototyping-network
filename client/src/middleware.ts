3f57e5c87ebc25f6e3a89ba2947353e8e9b0162f
anamontiaga <amontiaga@gmail.com>
2024-05-21 13:11:18 +0200

    add default locale

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,14 +1,21 @@
-import createMiddleware from 'next-intl/middleware';
+import { NextRequest } from 'next/server';
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'es', 'pt', 'id'],
+import createIntlMiddleware from 'next-intl/middleware';
 
-  // Used when no locale matches
-  defaultLocale: 'en',
-});
+export default async function middleware(request: NextRequest) {
+  const defaultLocale = request.headers.get('x-your-custom-locale') || 'en';
+
+  const handleI18nRouting = createIntlMiddleware({
+    locales: ['en', 'es', 'pt', 'id'],
+    defaultLocale,
+  });
+  const response = handleI18nRouting(request);
+
+  response.headers.set('x-your-custom-locale', defaultLocale);
+
+  return response;
+}
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(en|es|pt|id)/:path*'],
+  matcher: ['/', '/(id|pt|es|en)/:path*'],
 };
